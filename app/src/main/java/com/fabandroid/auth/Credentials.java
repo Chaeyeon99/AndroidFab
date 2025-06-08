e42842464508c8d13cc929afd687e0ab9f5f0643
Chaeyeon <74848401+Chaeyeon99@users.noreply.github.com>
2025-06-08 15:38:58 +0900

    Update Credentials.java

--- app/src/main/java/com/fabandroid/auth/Credentials.java
+++ app/src/main/java/com/fabandroid/auth/Credentials.java
@@ -1,7 +1,7 @@
 package com.fabandroid.auth;
 
 public class Credentials {
-    public static final String EMAIL = "androiduser@naver.com";
+    public static final String EMAIL = "androidtesting@samsung.com";
     public static final String PASSWORD = "fabAndroidSecure123";
     public static final String ACCESS_TOKEN = "android-access-token-fab";
     public static final String REFRESH_TOKEN = "android-refresh-token-fab";
